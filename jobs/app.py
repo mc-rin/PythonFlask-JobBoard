f5c6bf9d9b1697f3919b63be7478556b01b18c9e
Jordan McLaurin <35611583+mc-rin@users.noreply.github.com>
2019-07-11 20:49:30 -0400

    Completed the eighth module

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,7 @@
 import sqlite3
+import datetime
 # g provides access to the database throughout the application
-from flask import Flask, g, render_template
-
+from flask import Flask, g, render_template, request, redirect, url_for
 PATH = 'db/jobs.sqlite'
 app = Flask(__name__)
 
@@ -74,3 +74,21 @@ def employer(employer_id):
                           'review.employer_id WHERE employer.id = ?',
                           [employer_id])
     return render_template('employer.html', employer=employer, jobs=jobs, reviews=reviews)
+
+
+@app.route('/employer/<employer_id>/review', methods=('GET', 'POST'))
+def review(employer_id):
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title = request.form['title']
+        status = request.form['status']
+        # Gets the current time
+        date = datetime.datetime.now().strftime('%m/%d/%Y')
+
+        execute_sql('INSERT INTO review (review, rating, title, date, '
+                    'status, employer_id) VALUES (?, ?, ?, ?, ?, ?)',
+                    (review, rating, title, date, status, employer_id),
+                    commit=True)
+        return redirect(url_for('employer', employer_id=employer_id))
+    return render_template('review.html', employer_id=employer_id)
